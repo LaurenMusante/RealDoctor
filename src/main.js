1ce4950aa4a1683d48c781f72f7ccad93081bc4d
Lauren <Lauren.Musante@gmail.com>
2019-09-14 10:57:04 -0700

    Initialize project and install Webpack. Create .env file with new API key

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import $ from 'jQuery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css'
+
+$(document).ready(function(){
+  $(formID).submit(function(event){
+    event.preventDefault();
+  })
+});
