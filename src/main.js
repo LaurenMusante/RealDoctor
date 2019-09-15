285a4ea575461a116985dbe7e49aab3dbb2cfcea
Lauren <Lauren.Musante@gmail.com>
2019-09-15 13:54:08 -0700

    Remove console logs

--- src/main.js
+++ src/main.js
@@ -12,12 +12,10 @@ $(document).ready(function(){
     let newPromise = newInfo.getDoctorInfo(searchedItem);
     newPromise.then(function(response){
       const body = JSON.parse(response);
-      console.log(body);
       let doctorFirstName = body.data[0].profile.first_name;
       let doctorLastName = body.data[0].profile.last_name;
       let address = body.data[0].practices[0].visit_address.street;
       let specialty = body.data[0].specialties[0].name;
-      console.log(address);
       $("#results").show();
       $("#info").append(`Your doctor's name: ${doctorFirstName} ${doctorLastName} <br>`)
       $("#info").append(`Visit your doctor at: ${address} in Portland, OR <br>`);
