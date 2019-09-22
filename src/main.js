739e45b8af417993e28ddac090dac0b0ea07f090
Lauren <Lauren.Musante@gmail.com>
2019-09-22 09:37:22 -0700

    Add forEach loop to cycle through doctors. Add function to clear previous search data before new search.

--- src/main.js
+++ src/main.js
@@ -7,24 +7,22 @@ import { DoctorInfo } from './scripts.js';
 $(document).ready(function(){
   $("#doctorForm").submit(function(event){
     event.preventDefault();
+    $("#info").text("");
     const searchedItem = $("#userInput").val();
     let newInfo = new DoctorInfo();
     let newPromise = newInfo.getDoctorInfo(searchedItem);
     newPromise.then(function(response){
       const body = JSON.parse(response);
-      let doctorFirstName = body.data[0].profile.first_name;
-      let doctorLastName = body.data[0].profile.last_name;
-      let address = body.data[0].practices[0].visit_address.street;
-      let specialty = body.data[0].specialties[0].name;
+      body.data.forEach(function(doctor){
+      let doctorFirstName = doctor.profile.first_name;
+      let doctorLastName = doctor.profile.last_name;
+      let address = doctor.practices[0].visit_address.street;
+      let specialty = doctor.specialties[0].name;
       $("#results").show();
       $("#info").append(`Your doctor's name: ${doctorFirstName} ${doctorLastName} <br>`)
       $("#info").append(`Visit your doctor at: ${address} in Portland, OR <br>`);
-      $("#info").append(`This doctor's specialty is listed as: ${specialty}`)
-      // 
-      // for (i=0; i > body.data.length, i++) { // need to go through data array in order to find more than the same doctor over and over again.
-      //
-      // }
-
+      $("#info").append(`This doctor's specialty is listed as: ${specialty} <br>`)
+      })
     });
   });
 });
--- src/scripts.js
+++ src/scripts.js
@@ -2,7 +2,7 @@ export class DoctorInfo {
 getDoctorInfo(searchedItem) {
   return new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?%20&skip=0&query=${searchedItem}limit=10&user_key=${process.env.exports.apiKey}&location=or-portland`;
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?%20&skip=0&query=${searchedItem}&limit=10&user_key=${process.env.exports.apiKey}&location=or-portland`;
     console.log(url);
     request.onload = function () {
       if (this.status === 200) {
@@ -14,5 +14,5 @@ getDoctorInfo(searchedItem) {
     request.open("GET", url, true);
     request.send();
      });
-  } //funciton closing curly
-} //class closing curly
+  } 
+}
